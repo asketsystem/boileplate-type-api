eb8ee083db42cec1339dbff40599915c6dade642
Franck Asket <32412954+asketsystem@users.noreply.github.com>
2022-08-02 11:58:42 +0000

    basic components

---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import './header.css'
+
+interface IHeaderProps { }
+
+const Header: React.FC<IHeaderProps> = () => {
+    return (
+        <div className="navbar">
+            <h3>React TypeScript</h3>
+        </div>
+    )
+
+}
+
+export default Header
