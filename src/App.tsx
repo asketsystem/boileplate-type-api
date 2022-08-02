6c353224fa149e06b1ff5933f17b95e45d4f671d
Franck Asket <32412954+asketsystem@users.noreply.github.com>
2022-08-02 14:20:10 +0000

    passing notes array to the listnotes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import './App.css';
 import Header from './components/Header';
+import { useState } from 'react'
+import ListNotes from './components/ListNotes';
+import { Note } from './models/note.model'
 
 function App() {
+  const [notes, setNotes] = useState<Note[]>([
+    {
+      id: "1",
+      title: 'New Note',
+      body: 'This is a new note.'
+    }
+  ])
   return (
     <>
       <Header />
+      <div className="container">
+        <ListNotes notes={notes} />
+      </div>
     </>
   );
 }
